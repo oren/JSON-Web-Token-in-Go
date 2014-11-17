444b11917a868a8b48ba631a7b0fd1dba026412f
Oren <ogolan@yp.com>
2014-11-17 00:05:15 -0800

    client side templates

--- static/main.js
+++ static/main.js
@@ -3,11 +3,19 @@
 var $ = document.querySelector.bind(document);
 var xhr = require('xhr');
 var serialize = require('form-serialize');
+var tmpl = require('microtemplates');
+var fs = require('fs');
+var restrictedTmpl = fs.readFileSync('./static/templates/restricted.html', 'utf8');
+var homeTmpl = fs.readFileSync('./static/templates/home.html', 'utf8');
 
 document.addEventListener('DOMContentLoaded', function() {
+  var main = $('main');
 
   var submit = $('form input[type=submit]');
   submit.addEventListener('click', auth);
+
+  var restricted = $('.restricted');
+  restricted.addEventListener('click', renderRestricted.bind(undefined, main));
 
   function auth (e) {
     e.preventDefault();
@@ -37,4 +45,24 @@ function callAuth(formData) {
     console.log('code', resp.statusCode);
     console.log('body', resp.body);
   });
+}
+
+function renderRestricted(main, e) {
+  console.log('main', main);
+  e.preventDefault();
+  e.stopPropagation();
+  main.innerHTML = tmpl(restrictedTmpl)({});
+
+  var home = $('.home');
+  home.addEventListener('click', renderHome.bind(undefined, main));
+}
+
+function renderHome(main, e) {
+  console.log('main', main);
+  e.preventDefault();
+  e.stopPropagation();
+  main.innerHTML = tmpl(homeTmpl)({});
+
+  var restricted = $('.restricted');
+  restricted.addEventListener('click', renderRestricted.bind(undefined, main));
 }
